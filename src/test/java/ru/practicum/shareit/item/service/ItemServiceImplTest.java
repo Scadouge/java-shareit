11ac2712fd2ed9155978d569af678b280d92886c
Scadouge <scadouge@gmail.com>
2024-06-17 17:05:32 +0300

    test: добавить дополнительные тесты

--- src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
+++ src/test/java/ru/practicum/shareit/item/service/ItemServiceImplTest.java
@@ -16,21 +16,27 @@ import ru.practicum.shareit.item.model.Comment;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.storage.CommentRepository;
 import ru.practicum.shareit.item.storage.ItemRepository;
+import ru.practicum.shareit.request.model.Request;
+import ru.practicum.shareit.request.storage.RequestRepository;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.storage.UserRepository;
 
 import javax.validation.ValidationException;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 @Transactional
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 class ItemServiceImplTest {
+    private final RequestRepository requestRepository;
     private final BookingRepository bookingRepository;
     private final ItemRepository itemRepository;
     private final CommentRepository commentRepository;
@@ -41,17 +47,40 @@ class ItemServiceImplTest {
     void shouldCreateAndGetAndUpdateItem() {
         User otherUser = userRepository.save(new User(null, "User 1 name", "user1@mail.com"));
         User owner = userRepository.save(new User(null, "User 2 name", "user2@mail.com"));
-        Item item = itemService.create(new CreateItemArgs(owner.getId(), "Item name", "Item desc", true, null));
+        Request request = requestRepository.save(new Request(null, "desc", null, owner.getId()));
+        Item item = itemService.create(new CreateItemArgs(owner.getId(), "Item name", "Item desc", true, request.getId()));
         Item savedItem = itemRepository.findById(item.getId()).orElseThrow();
+
         assertEquals(savedItem, item);
         assertEquals(savedItem, itemService.get(item.getId(), owner.getId()));
         assertThat(itemService.getAll(owner.getId(), 0, 10), contains(item));
 
         itemService.update(new UpdateItemArgs(null, "Updated desc", null), item.getId(), owner.getId());
         Item updatedItem = itemRepository.findById(item.getId()).orElseThrow();
+
         assertEquals("Item name", updatedItem.getName());
         assertEquals("Updated desc", updatedItem.getDescription());
         assertThrows(NotAllowedException.class, () -> itemService.update(new UpdateItemArgs(null, "Updated desc", null), item.getId(), otherUser.getId()));
+    }
+
+    @Test
+    void shouldGetCorrectMappings() {
+        User booker = userRepository.save(new User(null, "User 1 name", "user1@mail.com"));
+        User owner = userRepository.save(new User(null, "User 2 name", "user2@mail.com"));
+        Request request = requestRepository.save(new Request(null, "desc", null, owner.getId()));
+        Item item = itemService.create(new CreateItemArgs(owner.getId(), "Item name", "Item desc", true, request.getId()));
+        bookingRepository.save(new Booking(null, item, BookingStatus.APPROVED, booker, LocalDateTime.now().minusDays(2), LocalDateTime.now().minusDays(1)));
+        Comment comment = commentRepository.save(new Comment(null, item.getId(), booker, "text", null));
+
+        Map<Long, List<Comment>> itemCommentMapping = itemService.getItemCommentMapping(Set.of(item.getId()));
+
+        assertThat(itemCommentMapping.entrySet(), hasSize(1));
+        assertThat(itemCommentMapping.get(item.getId()), equalTo(List.of(comment)));
+
+        Map<Long, List<Item>> requestItemMapping = itemService.getRequestItemMapping(Set.of(request.getId()));
+
+        assertThat(requestItemMapping.entrySet(), hasSize(1));
+        assertThat(requestItemMapping.get(request.getId()), equalTo(List.of(item)));
     }
 
     @Test
