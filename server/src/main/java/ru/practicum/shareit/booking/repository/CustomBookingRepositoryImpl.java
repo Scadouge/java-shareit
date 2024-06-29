83899900a947c9189f3d192796e013fd3acd1c75
Scadouge <scadouge@gmail.com>
2024-06-29 03:11:32 +0300

    refactor: удалить закомментированный код

--- server/src/main/java/ru/practicum/shareit/booking/repository/CustomBookingRepositoryImpl.java
+++ server/src/main/java/ru/practicum/shareit/booking/repository/CustomBookingRepositoryImpl.java
@@ -40,32 +40,4 @@ public class CustomBookingRepositoryImpl implements CustomBookingRepository {
                 .setParameter("status", status.toString())
                 .getResultList();
     }
-
-//    @Override
-//    public List<Booking> findLastBookingWithStatus(Set<Long> itemIds, LocalDateTime time, BookingStatus status) {
-//        String sql = "SELECT bk.* FROM bookings bk " +
-//                "INNER JOIN (SELECT bk.item_id, MAX(date_end) as max_date FROM bookings bk " +
-//                "WHERE bk.item_id IN :itemIds AND (bk.date_end < :time OR bk.date_start < :time AND bk.date_end > :time) AND bk.status = :status " +
-//                "GROUP BY bk.item_id) sb ON bk.item_id = sb.item_id AND bk.date_end = sb.max_date";
-//        return entityManager.createNativeQuery(sql, Booking.class)
-//                .setParameter("itemIds", itemIds)
-//                .setParameter("time", time)
-//                .setParameter("time", time)
-//                .setParameter("time", time)
-//                .setParameter("status", status.toString())
-//                .getResultList();
-//    }
-//
-//    @Override
-//    public List<Booking> findNextBookingWithStatus(Set<Long> itemIds, LocalDateTime time, BookingStatus status) {
-//        String sql = "SELECT bk.* FROM bookings bk " +
-//                "INNER JOIN (SELECT bk.item_id, MIN(date_start) as min_date FROM bookings bk " +
-//                "WHERE bk.item_id IN :itemIds AND bk.date_start > :time AND bk.status = :status " +
-//                "GROUP BY bk.item_id) sb ON bk.item_id = sb.item_id AND bk.date_start = sb.min_date";
-//        return entityManager.createNativeQuery(sql, Booking.class)
-//                .setParameter("itemIds", itemIds)
-//                .setParameter("time", time)
-//                .setParameter("status", status.toString())
-//                .getResultList();
-//    }
 }
